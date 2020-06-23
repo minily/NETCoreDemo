315a5f43812f07656d0bfb0c545ab7ec7e9c1bb6
minily <mililin136@qq.com>
2020-06-23 14:09:34 +0800

    NLog add ClearProviders

--- NLog008/Program.cs
+++ NLog008/Program.cs
@@ -39,7 +39,11 @@ namespace NLog008
                 {
                     webBuilder.UseStartup<Startup>();
                 })
-
+                .ConfigureLogging(logging =>
+                {
+                    logging.ClearProviders();
+                    logging.SetMinimumLevel(LogLevel.Trace);
+                })
                 // 日志使用方式都是一样的，即使Controller里以来注入的是ILogger
                 // 但.NET Core都会使用最后Use的日志类型，扩展封装的非常好，更换一种日志类型，项目其他地方不需要维护
                 .UseNLog();
